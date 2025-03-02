6fe15bab3283ec6874127a8fed3859825799b2bc
Irina Yukavskaya <strix1813@yandex.ru>
2025-03-02 04:48:38 +0300

    updated naming

--- database.sql
+++ database.sql
@@ -1,13 +1,13 @@
-\CREATE TABLE lessons (
+CREATE TABLE lessons (
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	name VARCHAR(50) NOT NULL,
-	body text,
-	video VARCHAR(500),
+	content text,
+	video_url VARCHAR(500),
 	position BIGINT,
 	created_at DATE NOT NULL,
-	update_at DATE,
+	updated_at DATE,
 	course_id BIGINT REFERENCES courses(id) NOT NULL,
-	is_deleted tinyint
+	deleted_at tinyint
 );
 
 CREATE TABLE courses (
@@ -15,9 +15,8 @@ CREATE TABLE courses (
         name VARCHAR(50) NOT NULL,
         description text,
         created_at DATE NOT NULL,
-        update_at DATE,
-	module_id BIGINT REFERENCES modules(id) NOT NULL,
-	is_deleted tinyint
+        updated_at DATE,
+	deleted_at tinyint
 );
 
 CREATE TABLE modules (
@@ -25,19 +24,27 @@ CREATE TABLE modules (
         name VARCHAR(50) NOT NULL,
         description text,
         created_at DATE NOT NULL,
-        update_at DATE,
-	program_id BIGINT REFERENCES programs(id) NOT NULL,
-	is_deleted tinyint
+        updated_at DATE,
+	deleted_at tinyint
 );
 
 CREATE TABLE programs (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         name VARCHAR(50) NOT NULL,
         price integer,
-	type VARCHAR(50),
+	program_type VARCHAR(50),
         created_at DATE NOT NULL,
-        update_at DATE,
-	is_deleted tinyint
+        updated_at DATE,
+);
+
+CREATE TABLE program_moduls (
+	program_id BIGINT REFERENCES programs(id) NOT NULL,
+	module_id BIGINT REFERENCES modules(id) NOT NULL
+);
+
+CREATE TABLE course_moduls (
+        course_id BIGINT REFERENCES courses(id) NOT NULL,
+        module_id BIGINT REFERENCES modules(id) NOT NULL
 );
 
 CREATE TABLE users (
@@ -47,17 +54,16 @@ CREATE TABLE users (
 	password VARCHAR(500),
 	teachig_groupe_id BIGINT REFERENCES teaching_groupes(id) NOT NULL,
         created_at DATE NOT NULL,
-        update_at DATE,
+        updated_at DATE,
 	role VARCHAR(50),
-        is_deleted tinyint
+	deleted_at tinyint
 );
 
 CREATE TABLE teaching_groups (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         slug VARCHAR(50) NOT NULL,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TYPE subscription AS ENUM ('active', 'pending', 'cancelled', 'completed');
@@ -65,21 +71,20 @@ CREATE TABLE enrollments (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	user_id BIGINT REFERENCES users(id) NOT NULL,
 	program_id BIGINT REFERENCES programs(id) NOT NULL,
-	subscription_status subscription,
+	status subscription,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TYPE pay AS ENUM ('pending', 'paid', 'failed', 'refunded');
 CREATE TABLE payments (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         enrollment_id BIGINT REFERENCES enrollments(id) NOT NULL,
-        payment_status pay,
-	payment_date DATE,
+	amount integer,
+        status pay,
+	payid_at DATE,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TYPE completion AS ENUM ('active', 'completed', 'pending', 'cancelled');
@@ -87,33 +92,30 @@ CREATE TABLE program_completions (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         user_id BIGINT REFERENCES users(id) NOT NULL,
 	program_id BIGINT REFERENCES programs(id) NOT NULL,
-        completion_status completion,
-        start_date DATE,
-	finish_date DATE,
+        status completion,
+        started_at DATE,
+	completed_at DATE,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TABLE certificates (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         user_id BIGINT REFERENCES users(id) NOT NULL,
         program_id BIGINT REFERENCES programs(id) NOT NULL,
-	certificate_url VARCHAR(500),
-	release_date DATE,
+	url VARCHAR(500),
+	issued_at DATE,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TABLE quizzes (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         lesson_id BIGINT REFERENCES lessons(id) NOT NULL,
         name VARCHAR(100),
-	body NVARCHAR,
+	content NVARCHAR,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TABLE exercises (
@@ -122,27 +124,25 @@ CREATE TABLE exercises (
         name VARCHAR(100),
         url VARCHAR(255),
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TABLE discussions (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         lesson_id BIGINT REFERENCES lessons(id) NOT NULL,
-        body NVARCHAR,
+	user_id BIGINT REFERENCES users(id) NOT NULL,
+        text NVARCHAR,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
 
 CREATE TYPE status AS ENUM ('created', 'in moderation', 'published', 'archived');
 CREATE TABLE blog (
         id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         user_id BIGINT REFERENCES users(id) NOT NULL,
-        title VARCHAR(255),
-        body text,
-	article_status status,
+        name VARCHAR(255),
+        content text,
+	status status,
         created_at DATE NOT NULL,
-        update_at DATE,
-        is_deleted tinyint
+        updated_at DATE
 );
